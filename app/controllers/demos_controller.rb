181f2cd206a8f05ea42b69cf52a820a6ccab0ef3
Muhammad Taimur <mrizwan@andrew.cmu.edu>
2019-04-24 13:37:53 +0000

    Before push

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -1,30 +1,35 @@
 class DemosController < ApplicationController
     
     layout 'login.html.erb'
-    def new 
-    end 
-    
-    def create 
-        user = Employee.find_by(email: params[:demo][:email])
-        if user && user.authenticate(params[:demo][:password])
-           login(user)
-            if params[:demo][:role] == 'employee'
-                redirect_to employee_hub_index_path
-            end
-            if params[:demo][:role] == 'manager'
-                redirect_to manager_hub_index_path
-            end
-            if params[:demo][:role] == 'admin'
-                redirect_to admin_hub_index_path
-            end
-            #redirect_to user
-        else
-           flash.now[:danger] = "Invalid email or password or role"
-           render 'new'
-        end
+    def new
     end
     
-    def destroy 
+    def create
+       user = User.find_by(email: params[:loginpage][:email].downcase) 
+       
+       if user && user.authenticate(params[:loginpage][:password])
+           login(user)
+           employee_user = Employee.find(user.user_employee_id)
+            if employee_user.role == 'admin'
+                redirect_to admin_hub_index_path 
+                return
+            
+            elsif employee_user.role == 'manager'
+                redirect_to manager_hub_index_path 
+                return
+            
+            else
+                redirect_to employee_hub_index_path 
+                return
+            end
+            
+       else
+           flash.now[:danger] = "Invalid email or password"
+           render 'new'
+       end
+    end
+    
+    def destroy
         logout
         redirect_to root_url
     end
